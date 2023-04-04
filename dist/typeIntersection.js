ccdb4aa6177cc733feb1fd6d02e7b9f12773fe24
dabasishJoy <dabasish@6sensehq.com>
2023-04-04 14:31:45 +0600

    insertion type

---
+++ dist/typeIntersection.js
@@ -0,0 +1,7 @@
+"use strict";
+let newBox = {
+    drag: () => 5,
+    resize: () => 10,
+};
+console.log(newBox.drag());
+//# sourceMappingURL=typeIntersection.js.map
---
+++ src/typeIntersection.ts
@@ -0,0 +1,24 @@
+// type intersaction is the opposite of union types where we require both type
+
+// the real time example is that
+
+// define type aliase
+type Dragable = {
+  drag: () => number;
+};
+
+// another type aliase
+type Resizeable = {
+  resize: () => number;
+};
+
+// create another type but combination of both dragable and resizeable
+type Box = Dragable & Resizeable; //Insertion type
+
+// create an object with the help of the Box type
+let newBox: Box = {
+  drag: () => 5,
+  resize: () => 10,
+};
+
+console.log(newBox.drag());
