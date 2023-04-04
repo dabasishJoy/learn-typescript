1cdb0f2d9ec0ae819813f831b4067062cee4cf49
dabasishJoy <dabasish@6sensehq.com>
2023-04-04 12:47:36 +0600

    arrays and tuples

---
+++ dist/tuples.js
@@ -0,0 +1,5 @@
+"use strict";
+let person;
+person = ["Joy", 10, true];
+console.log(person);
+//# sourceMappingURL=tuples.js.map
---
+++ src/tuples.ts
@@ -0,0 +1,10 @@
+// Tuple in ts
+
+// Tuple is a typed array with pre-defined type and length of each index
+
+// for example
+let person: [string, number, boolean]; //define tuple
+
+person = ["Joy", 10, true]; //create an array with defined tuple
+
+console.log(person);
