5e8b73c5ab8bba0e79a8da25a67508d4f6de9489
dabasishJoy <dabasish@6sensehq.com>
2023-04-04 12:49:28 +0600

    tuples

--- src/tuples.ts
+++ src/tuples.ts
@@ -3,8 +3,10 @@
 // Tuple is a typed array with pre-defined type and length of each index
 
 // for example
-let person: [string, number, boolean]; //define tuple
+let person: [string, number, boolean]; //define tuple -> fixed length and fixed type
 
 person = ["Joy", 10, true]; //create an array with defined tuple
+// person = ["Joy", "Alom", true]; //throw error for type
+person = ["Joy", 5, true, 5]; //throw error for length
 
 console.log(person);
